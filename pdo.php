d0505de853e2be29130002294f17445f9798a3c5
Francis Jules <francisjulescelesteespartero@gmail.com>
2022-02-14 21:20:56 +0800

    Added PHP file

---
+++ pdo.php
@@ -0,0 +1,9 @@
+<?php
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=login_profile', 'root', '');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+
+//port=8889 I used my port 3306 in localhost
+
+  
